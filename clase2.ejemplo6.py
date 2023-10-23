35eea545257557a541a8177392b42ddc03457fbb
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-23 12:46:26 -0300

    actualizando cambios

---
+++ clase2.ejemplo6.py
@@ -0,0 +1,3 @@
+# Realice un programa que pida un número de del 
+# día del 1 al 7 y presente el nombre del día que corresponde según la tabla: 
+# lunes=1, martes=2, miercoles=3, jueves=4, viernes=5, sabado=6, domingo=7
