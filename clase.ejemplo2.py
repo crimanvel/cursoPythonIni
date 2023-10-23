6abc2bf1ae179bd742bf2efa05160b80a95dc045
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-23 11:57:23 -0300

    actualizando cambios

---
+++ clase.ejemplo2.py
@@ -0,0 +1,4 @@
+# Pido al usuario una palabra y la presento en pantalla
+
+variable = input("Ingrese una palabra: ")
+print(variable)
---
+++ clase2.ejemplo1.py
@@ -0,0 +1,2 @@
+# Esto sirve para presentar algo en pantalla
+print("Hola está es la segunda clase")
