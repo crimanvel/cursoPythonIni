3302b5953fb7c8207878f082a806e5137b378516
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-23 12:43:41 -0300

    actualizando cambios

--- clase2.ejemplo5.py
+++ clase2.ejemplo5.py
@@ -1,4 +1,4 @@
-#Pida dos numeros enteros y calcule la suma (+), resta (-), multiplicación (*), division (/), mod (%) y div (//)
+# Pida dos numeros enteros y calcule la suma (+), resta (-), multiplicación (*), division (/), mod (%) y div (//)
 numero1 = int(input("Ingrese el primer numero: "))
 numero2 = int(input("Ingrese el segundo numero: "))
 
@@ -9,4 +9,17 @@ divi = numero1 / numero2
 mod = numero1 % numero2
 div = numero1 // numero2
 
-print("La suma es: ", sum, "La resta es: ", res, "La multiplicación es: ",mult, "La división es: ", divi, "El modulo es:", mod, "El div es: ", div)
+print(
+    "La suma es: ",
+    sum,
+    "La resta es: ",
+    res,
+    "La multiplicación es: ",
+    mult,
+    "La división es: ",
+    divi,
+    "El modulo es:",
+    mod,
+    "El div es: ",
+    div,
+)
