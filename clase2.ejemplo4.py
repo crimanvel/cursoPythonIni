f555619fe181058bb9fdb8634ae9ccbde2fbb860
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-23 12:09:14 -0300

    actualizando cambios

---
+++ clase2.ejemplo4.py
@@ -0,0 +1,7 @@
+# Pido dos números y los presento en pantalla
+
+numero1 = int(input("Dame el primero numero: "))
+numero2 = int(input("Dame el segundo numero: "))
+
+print("El primero numero es: ", numero1)
+print("El segundo numero es:", numero2)
