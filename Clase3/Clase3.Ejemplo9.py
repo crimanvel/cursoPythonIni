660318c6ed8af6e294bd86974b5cd411f6946aca
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-30 13:33:48 -0300

    clase3

---
+++ Clase3/Clase3.Ejemplo9.py
@@ -0,0 +1,17 @@
+#   Días de la semana: Lunes(1), Martes(2), Miercoles (3), Jueves (4), Viernes (5), Sábado (6), Domingo (7)
+dia = input("Ingrese el día: ")
+
+if dia == "Lunes" :
+    print(1)
+elif dia == "Martes":
+    print(2)
+elif dia == "Miercoles":
+    print(3)
+elif dia == "Jueves":
+    print(4)
+elif dia == "Viernes":
+    print(5)
+elif dia == "Sábado":
+    print(6)
+elif dia == "Domingo":
+    print(7)
