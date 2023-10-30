1b80d747853567a9c6b2916dfec86ffc508c5f8c
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-30 11:43:36 -0300

    clase 3

---
+++ Clase3/Clase.Ejemplo3.py
@@ -0,0 +1,19 @@
+#   Días de la semana: Lunes(1), Martes(2), Miercoles (3), Jueves (4), Viernes (5), Sábado (6), Domingo (7)
+numeroSemana = int(input("Indique un número entero entre 1 a 7: "))
+
+if numeroSemana == 1:
+    print("Lunes")
+elif numeroSemana == 2:
+    print("Martes")
+elif numeroSemana == 3:
+    print("Miercoles")
+elif numeroSemana == 4:
+    print("Jueves")
+elif numeroSemana == 5:
+    print("Viernes")
+elif numeroSemana == 6:
+    print("Sábado")
+elif numeroSemana == 7:
+    print("Domingo")
+else:
+    print("Número equivocado")
