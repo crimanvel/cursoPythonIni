dceab82c91d3763c6c9da546e7fb25f9c306a4d7
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-30 12:52:23 -0300

    clase3

--- Clase3/Clase3.Ejemplo6.py
+++ Clase3/Clase3.Ejemplo6.py
@@ -1,23 +1,20 @@
-n1 = int(input("Ingrese el primer número: "))
-n2 = int(input("Ingrese el segundo número: "))
-n3 = int(input("Ingrese el tercer número: "))
+# Dado tres número enteros distintos, encuentre el mayor de ellos
 
-if n1 >= n2 and n1 >= n3 or n1 == n2 == n3:
-    mayor = n1
-    print("El número mayor es:", mayor)
-elif n2 >= n1 and n2 >= n3:
-    mayor = n2
-    print("El número mayor es:", mayor)
-else:
-    mayor = n3
-    print("El número mayor es:", mayor)
+n1 = int(input("Ingrese el primer número entero: "))
+n2 = int(input("Ingrese el segundo número entero: "))
+n3 = int(input("Ingrese el tercer número entero: "))
 
-if n1 <= n2 and n1 <= n3 or n1 == n2 == n3:
-    menor = n1
-    print("El número menor es:", menor)
-elif n2 <= n1 and n2 <= n3:
-    menor = n2
-    print("El número menor es:", menor)
+if n1 < n2 < n3:
+    print("El número mayor es el tercer número")
+elif n1 < n3 < n2:
+    print("El número mayor es el segundo número")
+elif n2 < n1 < n3:
+    print("El número mayor es el tercero número")
+elif n2 < n3 < n2:
+    print("El número mayor es el primero número")
+elif n3 < n1 < n2:
+    print("El número mayor es el segundo número")
+elif n3 < n2 < n1:
+    print("El número mayor es el primero número")
 else:
-    menor = n3
-    print("El número menor es:", menor)
+    print("Algunos de los números son iguales")
