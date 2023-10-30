62da6659c8831d56ef69d8df1cfb30f26f261c65
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-30 13:14:53 -0300

    clase3

---
+++ Clase3/Clase3.Ejemplo7.py
@@ -0,0 +1,16 @@
+# Ciclo While
+
+#Presente los números del 1 al 20
+
+indice = 1
+
+while indice < 21 :
+    print(indice)
+    indice = indice + 1 
+
+# Presente en pantalla los números del 20 al 1
+print("Los números son: ")
+indice = 20
+while 0 < indice:
+    print(indice)
+    indice = indice -1
