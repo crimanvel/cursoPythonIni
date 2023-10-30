6c904bcbaeec3b45f6c7247908d4edb7b02179d9
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-10-30 11:34:51 -0300

    clase 3

---
+++ Clase3/Clase3.Ejemplo1.py
@@ -0,0 +1,16 @@
+# Repaso
+# Imprime lo que esta entre comillas
+print("Lo que uno quiere imprimir")
+# Leer una oración e imprimirla en pantalla
+texto = input("Ingrese una oración")
+print(texto)  # fijense que la palabra texto no va entre comillas
+print("texto")
+# Quiero leer un número e imprimirlo en pantalla, debo saber si es entero o real
+# Si es entero (sin coma) va con int
+# Si es real (con coma) va con float
+# Ingresamos un número entero y lo presentamos en pantalla
+edad = int(input("Ingrese su edad: "))
+print(edad)
+# Ingresamos un numero real y lo presentamos en pantalla
+altura = float(input("Ingrese su altura en metros: "))
+print(altura)
---
+++ Clase3/Clase3.Ejemplo2.py
@@ -0,0 +1,11 @@
+#Ciclo if
+# if pregunta :
+    #si la pregunta es verdadera sigue aqui
+#else:
+    # si la pregunta es falsa sigue aqui
+#Pido que ingrese un número que sea 1 o 0 y luego presento en palabra que número ingreso.
+numero= int(input("Ingrese un número que sea 0 o 1: "))
+if numero == 1:
+    print("El número ingresado es un uno.")
+else:
+    print("El número ingresado es un cero.")
