1b50394092f85f5d3c29d34dca9128859101762a
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-11-06 11:53:01 -0300

    clase 4

---
+++ Clase4/Ejercicio3.py
@@ -0,0 +1,8 @@
+#pedir dos numeros enteros presentar los números enteros entre los dos
+n1= int(input("Ingrese el primer número: "))
+n2= int(input("Ingrese el segundo número (que es mayor que el primero): "))
+n1=n1+1
+while n1<n2 :
+    print(n1)
+    n1 = n1 +1    
+print("fin") 
