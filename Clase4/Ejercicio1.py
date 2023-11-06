eb7cab70a4ecb565f80140ebdb3e2b4a0c76fdd3
Cristian Manuel Veliz <cristianmanuelveliz@gmail.com>
2023-11-06 11:35:30 -0300

    clase 4

---
+++ Clase4/Ejercicio1.py
@@ -0,0 +1,13 @@
+# Ciclo de repeteción while do, trabaja asi:
+# while condición : debe ser V o falsa, si se cumple la condición
+#                   si se cumple la condición sigue aqui
+#                   si no se cumple sigue aqui
+# Presente en pantalla los números del 1 al 10
+
+numero = 1
+while numero <= 10:
+    print(numero)  # 1,2,3,...,10
+    numero = numero + 1
+print("Fin")
+
+
---
+++ Clase4/Ejercicio2.py
@@ -0,0 +1,6 @@
+# Presente en pantalla los números del 10 al 1
+n = 10
+while n >= 1:
+    print(n)  # 10,9,8,...,1
+    n = n - 1
+print("Fin")
